ac6cb8ca13774cdd1564fedd38cdc570eb2f3929
alexisOclock <alexis@oclock.io>
2022-03-31 15:12:29 +0200

    base pour l'exo

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -16,12 +16,11 @@ class Converter extends React.Component {
     // avec les données initiales pour contrôler/définir ce qu'on veut afficher dans l'application
     this.state = {
       baseAmount: 1,
-      isOpen: false,
+      isOpen: true,
     };
   }
 
   render() {
-    console.log('render', this.state);
     // je peux lire mon state
     const baseAmount = this.state.baseAmount;
     const isOpen = this.state.isOpen;
@@ -36,10 +35,27 @@ class Converter extends React.Component {
       });
     };
 
+    // ici pour éviter de perdre la référence à l'instance pour this, j'ai fait une fonction fléchée
+    // en effet les fléchées concervent le this de là ou elles sont écrit, ici la classe
     const increment = () => {
       this.setState({
         baseAmount: baseAmount + 1,
       });
+    };
+
+    // fonction qui fait la conversion qu'il faudra sans doute appeler à un moment
+    const makeConversion = () => {
+      // on part de la devise actuelle qui doit être ajouté au state
+      const { currency } = this.state;
+      // Récupération des infos de la devise choisie
+      const currencyData = listOfCurrencies.find((data) => data.name === currency);
+      // console.log(currencyData);
+      // Extraction du taux
+      const { rate } = currencyData;
+      // Calcul du résultat
+      const result = baseAmount * rate;
+      // On retourne avec une petite mise en forme
+      return Math.round(result * 100) / 100;
     };
 
     // je configure ma représentation en fonction de mon state
