41efa96faa30e92f209b23fa054246d90f44cdcb
alexisOclock <alexis@oclock.io>
2022-03-31 11:07:08 +0200

    props et defaultProps

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,25 @@
+import PropTypes from 'prop-types';
 import './style.scss';
 
-function Header() {
+function Header({ amount }) {
+  let currency = 'euro';
+  if (amount > 1) {
+    currency += 's';
+  }
   return (
     <header className="header">
       <h1 className="header-title">Converter</h1>
-      <p className="header-amount">1 euro</p>
+      <p className="header-amount">{amount} {currency}</p>
     </header>
   );
 }
+
+Header.propTypes = {
+  amount: PropTypes.number,
+};
+
+Header.defaultProps = {
+  amount: 1,
+};
 
 export default Header;
